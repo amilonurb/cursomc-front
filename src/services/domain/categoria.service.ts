002ccb737d979fc4c26994cb3bcc0aa91fd135d7
amilonurb <brunolima.18@outlook.com>
2020-01-19 21:50:27 -0300

    Adicionando serviço para listar Categorias

---
+++ src/services/domain/categoria.service.ts
@@ -0,0 +1,18 @@
+import { CategoriaDTO } from './../../models/categoria.dto';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { Observable } from 'rxjs/Rx';
+
+import { API_CONFIG as api } from '../../config/api.config';
+
+
+@Injectable()
+export class CategoriaService {
+
+    constructor(public httpClient: HttpClient) { }
+
+    findAll(): Observable<CategoriaDTO[]> {
+        return this.httpClient.get<CategoriaDTO[]>(`${api.baseURL}/categorias`);
+    }
+}
