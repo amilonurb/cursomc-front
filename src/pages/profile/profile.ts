e6fe5ab889e1751df6a4f83895873d8deb463926
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2020-02-04 20:53:59 -0200

    Opção para obter a imagem da galeria

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -72,4 +72,22 @@ export class ProfilePage {
             this.cameraOn = false;
         }, (error) => { });
     }
+
+    getGalleryPicture() {
+
+        this.cameraOn = true;
+
+        const options: CameraOptions = {
+            quality: 100,
+            sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+            destinationType: this.camera.DestinationType.DATA_URL,
+            encodingType: this.camera.EncodingType.PNG,
+            mediaType: this.camera.MediaType.PICTURE
+        }
+
+        this.camera.getPicture(options).then((imageData) => {
+            this.picture = 'data:image/png;base64,' + imageData;
+            this.cameraOn = false;
+        }, (error) => { });
+    }
 }
